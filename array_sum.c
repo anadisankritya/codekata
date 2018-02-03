a30cea1b7791568edb9ca1cddc18d2c2abad3bea
anadisankritya <anadisankritya@gmail.com>
2018-02-03 11:46:49 +0530

    Create array_sum.c

---
+++ array_sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,k,s=0;
+    scanf("%d%d",&n,&k);
+    int a[n];
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<k;i++)
+        s=s+a[i];
+    printf("%d",s);
+    return 0;
+}
